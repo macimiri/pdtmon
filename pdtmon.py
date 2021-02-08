1b5f3a32669c9933a0f7b250988bfcf5258c99db
maci <mgamer1234@gmail.com>
2021-02-08 11:34:30 -0800

    Add graphics config to hide mouse cursor. Split update paths.
    Hide mouse so it's not showing on display. (Assume keyboard still functional to switch tab and kill process if needed.)
    Also split update paths in 2 for things that update each minute (current time, daily transitions) and things that update each day-ish (yearly transitions, day length)

--- pdtmon.py
+++ pdtmon.py
@@ -8,6 +8,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.config import Config
 from kivy.properties import StringProperty
+from kivy.properties import NumericProperty
 from kivy.clock import Clock
 
 '''
@@ -31,7 +32,7 @@ the daylight savings "utc_transition dates" from pytz only go to 2037 :(
 
 class screen(Widget):
     # all of the variables that the ui pulls data from
-    alpha = 0.5
+    alpha = NumericProperty()
     otto_info = {'lat': 38.662644, 'long': -121.527298, 'tz':'US/Pacific'}
     otto_obs = astral.Observer(latitude=otto_info['lat'], longitude=otto_info['long'])
 
@@ -41,6 +42,7 @@ class screen(Widget):
     yesterday_today_delta_str = StringProperty()
     today_tomorrow_delta_str = StringProperty()
     date = StringProperty()
+    current_time = StringProperty()
 
     day_transition_1 = StringProperty()
     day_transition_2 = StringProperty()
@@ -49,40 +51,25 @@ class screen(Widget):
     year_transition_1 = StringProperty()
     year_transition_2 = StringProperty()
 
-    # update all of the numbers
-    def update_display(self):
-        # get sun times for 3 days
-        otto_sun_yesterday = astral.sun.sun(self.otto_obs, datetime.datetime.today() - datetime.timedelta(days=1), tzinfo=self.otto_info['tz'])
-        otto_sun_today = astral.sun.sun(self.otto_obs, datetime.datetime.today(), tzinfo=self.otto_info['tz'])
-        otto_sun_tomorrow = astral.sun.sun(self.otto_obs, datetime.datetime.today() + datetime.timedelta(days=1), tzinfo=self.otto_info['tz'])
+    # update all of the numbers that change by-the-minute
+    def update_minute_info(self):
+        # current time, used in many of following calculations
+        rn = datetime.datetime.now(tz=pytz.timezone(self.otto_info['tz']))
+        self.current_time = rn.strftime('%H:%M')
 
-        # compute day lengths
-        day_length_yesterday = otto_sun_yesterday['sunset'] - otto_sun_yesterday['sunrise']
-        self.day_length_yesterday_str = self.daylen_tostr(day_length_yesterday)
-        day_length_today = otto_sun_today['sunset'] - otto_sun_today['sunrise']
-        self.day_length_today_str = self.daylen_tostr(day_length_today)
-        day_length_tomorrow = otto_sun_tomorrow['sunset'] - otto_sun_tomorrow['sunrise']
-        self.day_length_tomorrow_str = self.daylen_tostr(day_length_tomorrow)
-
-        #compute how much longer today was vs yesterday, change delta text color
-        self.yesterday_today_delta_str = self.daydelta_tostr(day_length_today - day_length_yesterday)
-        if self.yesterday_today_delta_str[0] == '+':
-            self.ids.yest_delta.color = (0,1,0,self.alpha)
+        # update alpha brightness according to time of day
+        if rn.time() < datetime.time(6):
+            self.alpha = 0.25
+        elif rn.time() < datetime.time(22):
+            self.alpha = 0.80
         else:
-            self.ids.yest_delta.color = (1,0,0,self.alpha)
+            self.alpha = 0.25
 
-        #compute how much longer tomorrow will be vs today, change delta text color
-        self.today_tomorrow_delta_str = self.daydelta_tostr(day_length_tomorrow - day_length_today)
-        if self.today_tomorrow_delta_str[0] == '+':
-            self.ids.tom_delta.color = (0,1,0,self.alpha)
-        else:
-            self.ids.tom_delta.color = (1,0,0,self.alpha)
-
-        # update today's date
-        self.date = datetime.date.today().strftime('%Y.%m.%d')
+        # TODO: change references from datetime.datetime.today() to rn
+        otto_sun_today = astral.sun.sun(self.otto_obs, rn, tzinfo=self.otto_info['tz'])
+        otto_sun_tomorrow = astral.sun.sun(self.otto_obs, rn + datetime.timedelta(days=1), tzinfo=self.otto_info['tz'])
 
         # find next 2 important daily transitions
-        rn = datetime.datetime.now(tz=pytz.timezone('America/Los_Angeles'))
         if rn < otto_sun_today['dawn']:  # rn before today.dawn
             #transitions are today.dawn, today.dusk
             self.day_transition_1 = f'dawn {otto_sun_today["dawn"].strftime("%H:%M")}'
@@ -101,6 +88,41 @@ class screen(Widget):
             self.ids.lbl_daymom_tom_1.color = (1,1,0,self.alpha)
             self.day_transition_2 = f'dusk {otto_sun_tomorrow["dusk"].strftime("%H:%M")}'
             self.ids.lbl_daymom_tom_2.color = (1,1,0,self.alpha)
+
+    # update all of the numbers that change once per day
+    def update_daily_info(self):
+        # current time, used in many of following calculations
+        rn = datetime.datetime.now(tz=pytz.timezone(self.otto_info['tz']))
+
+        # get sun times for 3 days
+        otto_sun_yesterday = astral.sun.sun(self.otto_obs, datetime.datetime.today() - datetime.timedelta(days=1), tzinfo=self.otto_info['tz'])
+        otto_sun_today = astral.sun.sun(self.otto_obs, datetime.datetime.today(), tzinfo=self.otto_info['tz'])
+        otto_sun_tomorrow = astral.sun.sun(self.otto_obs, datetime.datetime.today() + datetime.timedelta(days=1), tzinfo=self.otto_info['tz'])
+
+        # compute day lengths
+        day_length_yesterday = otto_sun_yesterday['sunset'] - otto_sun_yesterday['sunrise']
+        self.day_length_yesterday_str = self.daylen_tostr(day_length_yesterday)
+        day_length_today = otto_sun_today['sunset'] - otto_sun_today['sunrise']
+        self.day_length_today_str = self.daylen_tostr(day_length_today)
+        day_length_tomorrow = otto_sun_tomorrow['sunset'] - otto_sun_tomorrow['sunrise']
+        self.day_length_tomorrow_str = self.daylen_tostr(day_length_tomorrow)
+
+        # compute how much longer today was vs yesterday, change delta text color
+        self.yesterday_today_delta_str = self.daydelta_tostr(day_length_today - day_length_yesterday)
+        if self.yesterday_today_delta_str[0] == '+':
+            self.ids.yest_delta.color = (0,1,0,self.alpha)
+        else:
+            self.ids.yest_delta.color = (1,0,0,self.alpha)
+
+        # compute how much longer tomorrow will be vs today, change delta text color
+        self.today_tomorrow_delta_str = self.daydelta_tostr(day_length_tomorrow - day_length_today)
+        if self.today_tomorrow_delta_str[0] == '+':
+            self.ids.tom_delta.color = (0,1,0,self.alpha)
+        else:
+            self.ids.tom_delta.color = (1,0,0,self.alpha)
+
+        # update today's date
+        self.date = rn.strftime('%Y.%m.%d')
 
         # compute PST or PDT completion percentage
         daylight_trans_dates = [x.date() for x in pytz.timezone('US/Pacific')._utc_transition_times
@@ -155,12 +177,15 @@ class screen(Widget):
 class pdtmonApp(App):
     def build(self):
         rtn = screen()
-        rtn.update_display()
-        Clock.schedule_interval(lambda dt: rtn.update_display(), 60*10)  # update every 10 min. randomly chosen
+        rtn.update_minute_info()
+        Clock.schedule_interval(lambda dt: rtn.update_minute_info(), 5)  # update every 5 sec for a somewhat reliable thing
+        rtn.update_daily_info()
+        Clock.schedule_interval(lambda dt: rtn.update_daily_info(), 60 * 10)  # update every 10 min. randomly chosen
         return rtn
 
 if __name__ == '__main__':
     Config.set('graphics', 'width', '1024')
     Config.set('graphics', 'height', '600')
+    Config.set('graphics', 'show_cursor', 0)
     #Config.set('graphics', 'fullscreen', 1)
     pdtmonApp().run()
